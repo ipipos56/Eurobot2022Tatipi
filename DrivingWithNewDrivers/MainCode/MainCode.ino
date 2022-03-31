50a0807c8907bd2d4fe89f4c473b6a20f66c2d17
JoshLeonid <tgazizullin@mail.ru>
2022-03-31 16:35:45 +0300

    Update MainCode.ino

--- DrivingWithNewDrivers/MainCode/MainCode.ino
+++ DrivingWithNewDrivers/MainCode/MainCode.ino
@@ -38,6 +38,7 @@ Adafruit_BNO055 bno = Adafruit_BNO055(-1, 0x29);
 
 #define BNO055_SAMPLERATE_DELAY_MS (50)
 
+String externalData = "";
 int staticAngle = 0;
 float lengthOfRobot = 0;
 float alpha = 0;
@@ -48,12 +49,15 @@ extern volatile unsigned long timer0_millis;
 
 void(* resetFunc) (void) = 0;
 void CalculateSpeed(int _angle);
-void move(int angle, long int length);
+int decoderOfOperation(int status);
+int move(int angle, long int length);
 int decoder(String _data);
+int normilize(int angle);
+int rotationWithPID(int _angle);
 String uart();
-void decoderOfOperation(int status);
 
-void setup() {
+void setup() 
+{
   timer0_millis = UINT32_MAX - 5000;
   Serial.begin(115200);
   roboclaw1.begin(115200);
@@ -69,10 +73,9 @@ void setup() {
   }
   //bno.setExtCrystalUse(true);
 }
-void loop() {
+void loop() 
+{
   decoderOfOperation(decoder(uart()));
-  while (1);
-  //
 }
 void CalculateSpeed(int _angle)
 {
@@ -91,6 +94,76 @@ String uart()
   return data;
 }
 
+int rotationCam(int _angle)
+{
+  
+}
+
+int rotationWithPID(int _angle)
+{
+  float rotationErr = 0, previousRotationErr = 0;
+  float difRotation = 0, intRotation = 0, prRotation = 0;
+  float kDifRotation = 9, kIntRotation = 0.5, kPrRotation = 8;
+  float _dt = 10;
+  int controlAct = 0;
+  int logic = 0;
+  roboclaw1.SetEncM1(address1, 0);
+  roboclaw1.SetEncM1(address2, 0);
+  roboclaw1.SetEncM1(address3, 0);
+  while (logic == 0)
+  {
+    imu::Vector<3> euler = bno.getVector(Adafruit_BNO055::VECTOR_EULER);
+    int x = euler.x();
+    _angle = normilize(_angle);
+    int smn = _angle - x;
+    if (smn > 180)
+      smn -= 360;
+    else if (smn < -180)
+      smn += 360;
+    rotationErr = smn;
+    prRotation = rotationErr * kPrRotation;
+    difRotation = ((rotationErr - previousRotationErr) / _dt) * kDifRotation;
+    previousRotationErr = rotationErr;
+    intRotation = (intRotation + rotationErr * _dt) * kIntRotation;
+    if (rotationErr > 0)
+      controlAct = (int)(prRotation + difRotation + intRotation);
+    else
+      controlAct = (int)(prRotation + difRotation + intRotation);
+    controlAct = constrain(abs(controlAct), 0, 127);
+    if (rotationErr > 0)
+    {
+      roboclaw1.BackwardM1(address1, abs(controlAct));
+      roboclaw1.BackwardM1(address2, abs(controlAct));
+      roboclaw1.BackwardM1(address1, abs(controlAct));
+    }
+    else
+    {
+      roboclaw1.ForwardM1(address1, abs(controlAct));
+      roboclaw1.ForwardM1(address2, abs(controlAct));
+      roboclaw1.ForwardM1(address3, abs(controlAct));
+    }
+    int speedA = roboclaw1.ReadSpeedM1(address1);
+    int speedB = roboclaw1.ReadSpeedM1(address2);
+    int speedC = roboclaw1.ReadSpeedM1(address3);
+    if ((abs(speedA) == 0) && (abs(speedB) == 0) && (abs(speedC) == 0) && (abs(rotationErr) < 1.0))
+    {
+      roboclaw1.SpeedM1(address1, 0);
+      roboclaw1.SpeedM1(address2, 0);
+      roboclaw1.SpeedM1(address3, 0);
+      logic = 1;
+    }
+  }
+  return logic;
+}
+
+int normilize(int angle)
+{
+  angle = angle % 360;
+  if (angle < 0)
+    angle += 360;
+  return angle;
+}
+
 int decoder(String _data)
 {
   char* json = _data.c_str();
@@ -106,20 +179,20 @@ int decoder(String _data)
   }
 }
 
-void move(int angle, long int length)
+int move(int angle, long int length)
 {
   int logic = 0;
   roboclaw1.SetEncM1(address1, 0);
   roboclaw1.SetEncM1(address2, 0);
   roboclaw1.SetEncM1(address3, 0);
   lengthOfRobot = 0;
+  externalData = "";
   delay(100);
   while (logic == 0)
   {
     imu::Vector<3> euler = bno.getVector(Adafruit_BNO055::VECTOR_EULER);
     int x = euler.x();
     CalculateSpeed(x);
-    Serial.println(x);
     enc1 = roboclaw1.ReadEncM1(address1);
     enc2 = roboclaw1.ReadEncM1(address2);
     enc3 = roboclaw1.ReadEncM1(address3);
@@ -132,25 +205,27 @@ void move(int angle, long int length)
     DeserializationError error = deserializeJson(doc, json);
     if (!error)
     {
+      externalData = data;
       logic = 2;
     }
     if (lengthOfRobot >= length)
     {
+      externalData = "";
       logic = 1;
     }
     delay(1);
   }
-  //Serial.println("pridurok");
   roboclaw1.SpeedM1(address1, 0);
   roboclaw1.SpeedM1(address2, 0);
   roboclaw1.SpeedM1(address3, 0);
+  return logic;
 }
 
-void decoderOfOperation(int status)
+int decoderOfOperation(int status)
 {
-
   if (status == 0)
-  { imu::Vector<3> euler = bno.getVector(Adafruit_BNO055::VECTOR_EULER);
+  {
+    imu::Vector<3> euler = bno.getVector(Adafruit_BNO055::VECTOR_EULER);
     int x = euler.x();
     String send = "";
     send += '{';
@@ -176,10 +251,77 @@ void decoderOfOperation(int status)
   }
   else if (status == 1)
   {
-    Serial.println("Ya");
-    delay(50);
     String angleString = doc[String("a")].as<String>();
-    String lengthString = doc[String("l")].as<String>();
-    move(angleString.toInt(), lengthString.toInt());
+    String lengthString = doc[String("b")].as<String>();
+    int movingStaus = move(angleString.toInt(), lengthString.toInt());
+    delay(50);
+    imu::Vector<3> euler = bno.getVector(Adafruit_BNO055::VECTOR_EULER);
+    int x = euler.x();
+    String send = "";
+    send += '{';
+    send += '"';
+    send += 'r';
+    send += '"';
+    send += ':';
+    send += '"';
+    send += '0';
+    send += '"';
+    send += ',';
+    send += '"';
+    send += 'a';
+    send += '"';
+    send += ':';
+    send += '"';
+    send += String(x, DEC);
+    send += '"';
+    send += '}';
+    Serial.println(send);
+    if(movingStaus == 2)
+    {
+      decoderOfOperation(decoder(externalData));
+    }
+  }
+  else if (status == 2)
+  {
+
+  }
+  else if (status == 3)
+  {
+    String angleString = doc[String("a")].as<String>();
+    rotationWithPID(angleString.toInt());
+    imu::Vector<3> euler = bno.getVector(Adafruit_BNO055::VECTOR_EULER);
+    int x = euler.x();
+    String send = "";
+    send += '{';
+    send += '"';
+    send += 'r';
+    send += '"';
+    send += ':';
+    send += '"';
+    send += '0';
+    send += '"';
+    send += ',';
+    send += '"';
+    send += 'a';
+    send += '"';
+    send += ':';
+    send += '"';
+    send += String(x, DEC);
+    send += '"';
+    send += '}';
+    Serial.println(send);
+  }
+  else if (status == 4)
+  {
+     String angleString = doc[String("a")].as<String>();
+     
+  }
+  else if (status == 5)
+  {
+
+  }
+  else if (status == 6)
+  {
+
   }
 }
